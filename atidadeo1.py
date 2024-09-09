c04789c7b5659defbc22f120558edcba25e54602
henzoxz <henzomag2009@gmail.com>
2024-09-09 08:40:43 -0300

    soma dos valores

---
+++ atidadeo1.py
@@ -0,0 +1,9 @@
+# Crie um programa que receba dois números do usuário e exiba a soma desses números.
+
+valor1 = int(input ("digite o primeiro valor"))
+valor2 = int(input ("digite o segundo valor"))
+
+soma = valor1 + valor2
+
+print (f"a soma do valor é = {soma}")
+       
